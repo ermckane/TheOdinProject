b6f44958ec2425f8a2621db6223e450911c4c3d7
ermckane <ermckane@gmail.com>
2022-08-19 21:27:30 -0400

    following javascript basics

--- foundations/Javascript-Basics/fundamentals-pt2/practic.js
+++ foundations/Javascript-Basics/fundamentals-pt2/practic.js
@@ -1,12 +1,3 @@
-const button = document.querySelector('button');
-
-function greet() {
-    const name = prompt('What is your name?');
-    alert(`Hello ${name}, nice to see you!`);
-}
-
-button.addEventListener('click', greet);
-
 // Length property returns the length of a string
 
 let txt = 'The Rising will not stop with me Roque. I am just the figure of a tide.';
@@ -27,3 +18,46 @@ let part2 = str2.slice(-20,-13);
 console.log(part2);
 
 //substring() is similar to slice() but the start and end values less than 0 are treated as 0.
+let str3 = "Ragnar, Darrow, Pax, Sevro";
+let part3 = str3.substring(-3,14);
+console.log(part3);
+
+//substr() is similar to slice but the second parameter specifies the length of the extracted part.
+let str4 = "Ragnar, Darrow, Reaper, Lionheart";
+let part4 = str4.substr(8,6);
+console.log(part4);
+
+// replace() methods returns a new string by replacing a specified value from another string.
+
+let text = "Please find my son Holiday.";
+let newtext = text.replace('find','help');
+console.log(text);
+console.log(newtext);
+
+// to replace all matches, use a regular expression with a /g
+let text2 = 'help help help';
+let newtext2 = text2.replace(/help/g, 'die');
+console.log(text2);
+console.log(newtext2);
+
+
+// First code with button
+
+const button = document.querySelector('button');
+
+
+const hour = new Date().getHours();
+let greeting;
+    if (hour < 10) {
+        greeting = "Good morning";
+      } else if (hour < 20) {
+        greeting = "Good day";
+      } else {
+        greeting = "Good evening";
+      }
+function greet() {
+    const name = prompt('What is your name?');
+    alert(`${greeting} ${name}, nice to see you!`);
+}
+
+button.addEventListener('click', greet);
