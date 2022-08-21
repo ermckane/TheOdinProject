0827f23be2f5b4aa492ac8dedc31741bfe8762c7
ermckane <ermckane@gmail.com>
2022-08-21 12:50:21 -0400

    learning about functions

---
+++ foundations/Javascript-Basics/fundamentals-pt3/javascript.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    alert('hello');
+  }
+  
+  myFunction();
+  // calls the function once
+  
+
+  const myArray = ['I', 'love', 'chocolate', 'frogs'];
+  const madeAString = myArray.join(' ');
+  console.log(madeAString);
+
+  const madeAnotherString = myArray.join('|');
+  console.log(madeAnotherString);
